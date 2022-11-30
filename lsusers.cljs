b2b86f1b46c95bac419b6a9bdde9ce1cb9e834d7
Mike Longworth <mail@mikelongworth.co.uk>
2022-11-30 10:20:05 +0000

    Add parse-passwd-entry to utils

--- lsusers.cljs
+++ lsusers.cljs
@@ -16,29 +16,18 @@
                                              :description "list users"})]
             (bean (.parse_args parser (->js (vec *command-line-args*))))))
 
-(let [;https://medium.com/factory-mind/regex-tutorial-a-simple-cheatsheet-by-examples-649dc1c3f285
-      entry-re #"^([^:]*):([^:]*):([^:]*):([^:]*):([^:]*):([^:]*):([^:]*)"
-      gecos-re #"^([^,]*)?\,?([^,]*)?\,?([^,]*)?\,?([^,]*)?\,?([^,]*)?"]
-  (defn- parse-passwd [output]
-    (let [parse-entry (fn [entry]
-                        (let [m (zipmap [:entry :user :pw :uid :gid :gecos :home-dir :shell] (re-find entry-re entry))
-                              m (into m (map (fn [k v]
-                                               (when v
-                                                 [k v]))
-                                             [:name :room-number :work-phone :home-phone :other]
-                                             (rest (re-find gecos-re (:gecos m)))))]
-                          m))]
-      (map parse-entry (split output \newline)))))
+(defn- parse-passwd [output]
+  (map utils/parse-passwd-entry (split output \newline)))
 
 (defn list-users []
   (println)
   (p/let [output ($ "getent passwd | grep /home/")
           ;output ($ "getent passwd")
-          user-info (parse-passwd (-> output bean :stdout))]
-    (doseq [{:keys [user name] :as _e} user-info]
+          user-info (-> output str parse-passwd)]
+    (doseq [{:keys [user full-name] :as _e} user-info]
       ;(pprint _e)
-      (println (if name
-                 (str user ", " name)
+      (println (if full-name
+                 (str user ", " full-name)
                  user)))))
 
 (list-users)
